0adc72501d6782a523644b79894cf9926c136071
Tobelum <chairmo10@gmail.com>
2025-11-05 17:37:05 -0500

    Update made

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const sidebar = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
-});
